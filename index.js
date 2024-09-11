1345eb09cdd2d51a75a3a7eb5bb17558bcf9ab90
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-09-11 18:56:20 +0300

    Add export and import

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
+import Article from "./Article.mjs";
+import { mult } from "./math.mjs";
+
 console.log('hi form node');
 
 const number1 =2;
 const number2= 8;
 console.log(number1+number2);
 
-const mult = (n1,n2)=>n1*n2;
 console.log(mult(4,9));
 
-class Article{
-    constructor(content){
-        this.content = content;
-    }
-    render(){
-        return this.content;
-    }
-}  
+  
 const newArticle = new Article('Content for article')
 console.log(newArticle.render());
